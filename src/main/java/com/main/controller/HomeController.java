69fae036c9c5870340bef2de724c28608efcfb8a
chistaam <tampcps38349@gmail.com>
2025-06-23 21:44:09 +0700

    add login

--- src/main/java/com/main/controller/HomeController.java
+++ src/main/java/com/main/controller/HomeController.java
@@ -5,10 +5,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/home")
 public class HomeController {
-	@GetMapping
-	public String home() {
-		return "Home/Home";
+//	@GetMapping("/home")
+//	public String home() {
+//		return "View/Home";
+//	}
+
+	@GetMapping("/login")
+	public String login() {
+		return "Login/Dangky";
+	}
+
+	@GetMapping("/header")
+	public String header() {
+		return "Layout/header";
+	}
+	
+	@GetMapping("/footer")
+	public String footer() {
+		return "Layout/footer";
+	}
+	
+	@GetMapping("/index")
+	public String index() {
+		return "View/index";
+	}
+
+	@GetMapping("/bottom-navigation")
+	public String bottomNavigation() {
+		return "Layout/bottom-navigation";
 	}
 }
---
+++ src/main/java/com/main/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+	@GetMapping("/auth")
+	public String login(){
+        return "View/auth";
+    }
+}
