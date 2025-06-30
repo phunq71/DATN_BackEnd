46089acf72e04544554815256a7680eb38159ef0
chistaam <tampcps38349@gmail.com>
2025-06-30 19:47:21 +0700

    update cartService

--- src/main/java/com/main/service/CartService.java
+++ src/main/java/com/main/service/CartService.java
@@ -1,4 +1,15 @@
 package com.main.service;
 
+import com.main.entity.Cart;
+import com.main.entity.CartId;
+
+import java.util.List;
+
 public interface CartService {
+    public List<Cart> findAll();
+    public Cart findById(CartId id);
+    public CartId create(Cart cart);
+    public Cart update(CartId id,Cart cart);
+    public void deleteById(CartId id);
+    public boolean existById(CartId id);
 }
--- src/main/java/com/main/serviceImpl/CartServiceImpl.java
+++ src/main/java/com/main/serviceImpl/CartServiceImpl.java
@@ -1,7 +1,56 @@
 package com.main.serviceImpl;
 
+import com.main.entity.Cart;
+import com.main.entity.CartId;
+import com.main.repository.CartRepository;
+import com.main.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
-public class CartServiceImpl {
+public class CartServiceImpl implements CartService {
+
+    private final CartRepository cartRepository;
+
+    public CartServiceImpl(CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
+
+    @Override
+    public List<Cart> findAll() {
+        return cartRepository.findAll();
+    }
+
+    @Override
+    public Cart findById(CartId id) {
+        return cartRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public CartId create(Cart cart) {
+        return cartRepository.save(cart).getId();
+    }
+
+    @Override
+    public Cart update(CartId id, Cart cart) {
+        if(cartRepository.findById(id).isPresent()){
+            cartRepository.save(cart);
+            return cart;
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(CartId id) {
+        cartRepository.deleteById(id);
+    }
+
+    @Override
+    public boolean existById(CartId id) {
+        return cartRepository.existsById(id);
+    }
 }
